3e58a0538b23581b24d985e6a72aca029e823ec7
Leo Burgy <leo.burgy@gmail.com>
2021-07-21 10:10:01 +0200

    Archive old scripts

---
+++ scripts/centrack.py
@@ -0,0 +1,123 @@
+import operator
+from pathlib import Path
+import argparse
+
+import cv2
+import tifffile as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.feature import peak_local_max
+from skimage import img_as_float
+
+import pdb
+
+from utils import image_8bit_contrast, labelbox_annotation_load, label_coordinates
+from centrioles import centrioles_detect, centriole_assign
+
+def nuclei_extract(nuclei, nucleus_area_min=1000):
+    """
+    Extract the nuclei into contours.
+    :param nuclei:
+    :param nucleus_area_min:
+    :return:
+    """
+
+    # Apply the gaussian blurring filter with large kernel (1/16 of the image width)
+    image_w, image_h = nuclei.shape[-2:]
+    kernel_size = image_w // 32 + 1
+
+    blurred = cv2.GaussianBlur(nuclei, (kernel_size, kernel_size), 0)
+
+    # Threshold the image
+    threshold_mode = cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    ret, nuclei_thresh = cv2.threshold(blurred, 0, 255, threshold_mode)
+
+    # Find the contours
+    contours, hierarchy = cv2.findContours(nuclei_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    nuclei_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > nucleus_area_min]
+
+    return nuclei_thresh, nuclei_contours
+
+
+
+def args_parse():
+    parser = argparse.ArgumentParser(description='Centriole detector')
+
+    parser.add_argument('--path', type=str,
+                        help='the path to the dataset folder')
+    parser.add_argument('--nucleus-area-min', type=int,
+                        help='the maximal area in pixel for a nucleus')
+    parser.add_argument('--channels', type=int, nargs='*',
+                        help='Set the channel reference to use [0-4]')
+    parser.add_argument('--threshold-foci', type=int,
+                        help='Threshold for focus brightness (16bit depth)')
+    parser.add_argument('--distance', type=int,
+                        help='Minimal distance between two foci')
+    parser.add_argument('--kernel-size', type=int,
+                        help='Kernel size to smooth the centriole channel')
+
+    return vars(parser.parse_args())
+
+
+def main(args):
+    # pdb.set_trace()
+    path_dataset = Path(args['path'])
+    dataset_name = path_dataset.name
+    path_projections = path_dataset / 'projected'
+    path_out = path_dataset / 'out'
+
+    distance_min = args['distance']
+    threshold_foci = args['threshold_foci']
+    nucleus_area_min = args['nucleus_area_min']
+    kernel_size = args['kernel_size']
+    channels = args['channels']
+
+    channels_string = "".join([str(i) for i in channels])
+
+    # Collect the ome.tiff files
+    files = sorted(tuple(file for file in path_projections.iterdir()
+                         if file.name.endswith('.tif')
+                         if not file.name.startswith('.')))
+    file = files[0]
+    print('Loading')
+    data = tf.imread(file, key=range(4))
+
+    # Extract the DAPI channel
+    # nuclei = data[0, :, :]
+    # nuclei = cv2.convertScaleAbs(nuclei, alpha=255 / nuclei.max())
+    # nuclei_threshold, nuclei_contours = nuclei_extract(nuclei, nucleus_area_min)
+    #
+    # nuclei_8bit_bgr = cv2.cvtColor(nuclei, cv2.COLOR_GRAY2BGR)
+    # nuclei_annotated = cv2.drawContours(nuclei_8bit_bgr, nuclei_contours, -1, (0, 255, 0), cv2.FILLED)
+    # cv2.imwrite(str(path_out / f'{dataset_name}_1_nuclei.png'), nuclei_annotated)
+
+    centrioles = data[channels, :, :].max(0)
+
+    centrioles = cv2.medianBlur(centrioles, 3)
+
+    centrioles_8bit = image_8bit_contrast(centrioles)
+    cv2.imwrite(str(path_out / f'{dataset_name}_C{channels_string}.png'),
+                centrioles_8bit)
+
+    foci_coords = centrioles_detect(centrioles, threshold_foci, distance_min, kernel_size=kernel_size)
+
+    # pdb.set_trace()
+    labels = labelbox_annotation_load(path_dataset / 'annotation.json', file.stem + '.png')
+
+    for i, (r, c) in enumerate(foci_coords):
+        cv2.drawMarker(centrioles_8bit, (c, r), 255, cv2.MARKER_CROSS, 10, 2)
+
+    for i, label in enumerate(labels):
+        x, y = label_coordinates(label)
+        x, y = int(x), int(y)
+        cv2.circle(centrioles_8bit, (x, y), 10, 150, 2)
+        cv2.putText(img=centrioles_8bit, text=str(i), org=(x + 10, y + 10),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, thickness=2, color=150)
+    # pdb.set_trace()
+    cv2.imwrite(str(path_out / f'{dataset_name}_map_C{channels_string}_T{threshold_foci}_K{kernel_size}.png'),
+                centrioles_8bit)
+
+
+if __name__ == '__main__':
+    args = args_parse()
+    main(args)
--- scripts/test_channels.py
+++ scripts/test_channels.py
@@ -4,9 +4,9 @@ import numpy as np
 
 
 def main():
-    blue = cv2.imread('data/blue.tif', cv2.IMREAD_GRAYSCALE)
-    green = cv2.imread('data/green.tif', cv2.IMREAD_GRAYSCALE)
-    red = cv2.imread('data/red.tif', cv2.IMREAD_GRAYSCALE)
+    blue = cv2.imread('../data/blue.tif', cv2.IMREAD_GRAYSCALE)
+    green = cv2.imread('../data/green.tif', cv2.IMREAD_GRAYSCALE)
+    red = cv2.imread('../data/red.tif', cv2.IMREAD_GRAYSCALE)
 
     combined = np.zeros((512, 512, 3), np.uint8)
 
@@ -14,12 +14,12 @@ def main():
     combined[:, :, 1] = green
     combined[:, :, 2] = red
 
-    with tf.TiffWriter('data/combined_tif_bgr.tif') as tif:
+    with tf.TiffWriter('../data/combined_tif_bgr.tif') as tif:
         tif.write(blue, photometric='minisblack')
         tif.write(green, photometric='minisblack')
         tif.write(red, photometric='minisblack')
 
-    cv2.imwrite('data/combined_cv2_bgr.tif', combined)
+    cv2.imwrite('../data/combined_cv2_bgr.tif', combined)
 
 
 if __name__ == '__main__':
