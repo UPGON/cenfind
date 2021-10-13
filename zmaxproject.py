9516cce93ab5ad6f88239a6f168ac03712df3778
Leo Burgy <leo.burgy@gmail.com>
2021-10-13 12:42:56 +0200

    Use tifffile to sort dimensions

--- zmaxproject.py
+++ zmaxproject.py
@@ -1,6 +1,7 @@
 # Z-max projection
 
 import numpy as np
+import tifffile as tf
 from tqdm import tqdm
 from aicsimageio import AICSImage
 
@@ -17,9 +18,21 @@ if __name__ == '__main__':
     pbar = tqdm(files)
 
     for file in pbar:
-        field = AICSImage(file)
-        projected = field.data.max(axis=2)
-        projected = np.expand_dims(projected, 2)
+
+        with tf.TiffFile(file) as field:
+            metadata = field.ome_metadata
+            order = field.series[0].axes
+            data = field.asarray()
+
+        if order == 'ZCYX':
+            z, c, y, x = data.shape
+            data = data.reshape((c, z, y, x))
+
+        if len(data.shape) < 5:
+            data = np.expand_dims(data, 0)
+
+        projected = data.max(axis=2)
+
         file_name_core = file.name.removesuffix(''.join(file.suffixes))
 
         dest_name = f'{file_name_core}_max.ome.tif'
