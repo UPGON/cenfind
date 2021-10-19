0824b50e31970030395b9bd627ce3616f2a3306a
Leo Burgy <leo.burgy@epfl.ch>
2021-10-19 17:29:47 +0200

    Update

--- fetch_labelbox.py
+++ fetch_labelbox.py
@@ -1,14 +1,29 @@
+from pathlib import Path
+
 import labelbox
+import logging
 import pandas as pd
+
+logging.basicConfig(level=logging.INFO)
 
 with open('configs/labelbox_api_key.txt', 'r') as apikey:
     LB_API_KEY = apikey.readline()
     LB_API_KEY = LB_API_KEY.rstrip('\n')
 
+projects = {
+    'RPE1wt_CEP63+CETN2+PCNT_1': 'ckusghnn68fir0zehargo6jcd',
+    'RPE1wt_CEP152+GTU88+PCNT_1': 'ckuwn9zv52zi20zb2esdv14xp',
+    'RPE1wt_CP110+GTU88+PCNT_2': 'ckuph8qhf3evx0zagg5g5cwdb',
+}
+
+dataset_name = 'RPE1wt_CEP152+GTU88+PCNT_1'
+path_root = Path('/Volumes/work/epfl/datasets')
+path_dataset = path_root / dataset_name
+
 
 def main():
     lb = labelbox.Client(api_key=LB_API_KEY)
-    project_uid = 'ckuph8qhf3evx0zagg5g5cwdb'
+    project_uid = projects[dataset_name]
     project = lb.get_project(project_uid)
     labels = project.export_labels(download=True)
 
@@ -18,15 +33,15 @@ def main():
     for label_id, label in enumerate(labels):
         for c_id, channel_id in enumerate(channels_id):
             image_name = label['External ID']
-            print(label_id, c_id, image_name)
             image_name = image_name.split('/')[-1]
 
+            logging.info(f"{image_name}: {label_id}, {c_id}")
+
             if image_name.startswith('.'):
-                print(f"{image_name} skipped")
+                logging.warning(f"{image_name} skipped")
                 continue
 
             image_name = image_name.split('.')[0]
-            print(image_name)
 
             condition, markers, replicate, ds_row, ds_col, _, channel_code = image_name.split('_')
             markers = markers.split('+')
@@ -37,6 +52,7 @@ def main():
                 continue
             if 'objects' not in label['Label'].keys():
                 continue
+
             foci = label['Label']['objects']
 
             for focus_id, focus in enumerate(foci):
@@ -53,11 +69,11 @@ def main():
                               'x': x,
                               'y': y}
                 data_export.append(focus_dict)
+
     data_export_df = pd.DataFrame(data_export)
-    data_export_df.to_csv(f'out/export_{project_uid}.csv', index=False)
-    print(0)
+    data_export_df.to_csv(path_dataset / f'{dataset_name}_annotation.csv', index=False)
+    logging.info(f"Writing {len(data_export_df)} objects to {path_dataset / f'{dataset_name}_annotation.csv'}")
 
 
 if __name__ == '__main__':
     main()
-    print(0)
