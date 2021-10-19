bc1bbade1e5edf963e80fa4f9bb20bcc041a4867
Leo Burgy <leo.burgy@epfl.ch>
2021-10-19 17:28:31 +0200

    Add text on images

--- extract_channel.py
+++ extract_channel.py
@@ -1,11 +1,14 @@
 from pathlib import Path
+import logging
 
 import tifffile as tf
 import cv2
 
 from centrack.data import contrast
 
-dataset_name = 'RPE1wt_CEP63+CETN2+PCNT_1'
+logging.basicConfig(level=logging.DEBUG)
+
+dataset_name = 'RPE1wt_CEP152+GTU88+PCNT_1'
 
 condition, markers, replicate = dataset_name.split('_')
 markers_list = markers.split('+')
@@ -24,19 +27,20 @@ if __name__ == '__main__':
         name_core = field.name.rstrip('.ome.tif')
         data = tf.imread(field)
 
-        for c in range(data.shape[0]):
-            channel = markers_list[c]
-
+        for c, channel in enumerate(markers_list):
             path_projections_channel = path_dataset / 'projections_channel' / channel
 
             path_projections_channel.mkdir(exist_ok=True, parents=True)
             (path_projections_channel / 'tif').mkdir(exist_ok=True)
             (path_projections_channel / 'png').mkdir(exist_ok=True)
 
+            file_name_dst = f"{name_core}_C{c}"
+
             plane = data[c, :, :]
-            tf.imwrite(path_projections_channel / 'tif' / f"{name_core}_C{c}.tif", plane)
+            tf.imwrite(path_projections_channel / 'tif' / (file_name_dst + ".tif"), plane)
 
             contrasted = contrast(plane)
             inverted = cv2.bitwise_not(contrasted)
-            cv2.imwrite(str(path_projections_channel / 'png' / f"{name_core}_C{c}.png"), inverted)
-            print(f'Saved: {name_core} channel: {c}')
+            cv2.putText(inverted, f"{file_name_dst} ({channel})", (200, 200), cv2.QT_FONT_NORMAL, .8, color=10, thickness=2)
+            cv2.imwrite(str(path_projections_channel / 'png' / (file_name_dst + ".png")), inverted)
+            logging.info(f'Saved: {name_core} channel: {c}')
