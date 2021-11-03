e517d706b501d8ae7f276ac2b0303b33a976f201
Leo Burgy <leo.burgy@gmail.com>
2021-11-03 10:43:55 +0100

    Implement evaluation of rfcnn

--- evaluate_rfcnn.py
+++ evaluate_rfcnn.py
@@ -1,32 +1,39 @@
 from pathlib import Path
+import logging
 
 import cv2
 import pandas as pd
-from aicsimageio import AICSImage
-
+import tifffile as tf
 from centrack.data import contrast
 
-path_dataset = Path('/Users/leoburgy/epfl/training_data')
-path_annotation = Path('out/positions_foci.csv')
-path_annotation_pred = Path('/Users/leoburgy/epfl/leo_results')
+logging.basicConfig(level=logging.INFO)
 
-name_file = 'RPE1wt_CEP63+CETN2+PCNT_1_000_000_max_C2'
+path_dataset = Path('/Users/leoburgy/Dropbox/epfl/centriole_detection/test')
+path_annotation = Path('/Users/leoburgy/Dropbox/epfl/centriole_detection/annotations.csv')
+path_pred_annotation = Path('/Users/leoburgy/Dropbox/epfl/centriole_detection/predictions/annotations')
+path_pred_vis = Path('/Users/leoburgy/Dropbox/epfl/centriole_detection/predictions/visualisation')
+path_pred_vis.mkdir(exist_ok=True)
 
 
 def main():
     annot_actual = pd.read_csv(path_annotation)
-    annot_actual = annot_actual.loc[annot_actual['image_name'] == (name_file + '.png')]
-    annot_pred = pd.read_csv(path_annotation_pred / (name_file + '.ome.tif.csv'))
-    image = AICSImage(path_dataset / (name_file + '.ome.tif'))
-    plane = contrast(image.get_image_data().squeeze())
+    for image in path_dataset.iterdir():
+        name_file = image.stem
+        logging.info(name_file)
+        annot_image_actual = annot_actual.loc[annot_actual['image_name'] == name_file]
+        annot_image_pred = pd.read_csv(path_pred_annotation / (name_file + '.tif.csv'))
+        plane = tf.imread(path_dataset / (name_file + '.tif'))
+        plane = contrast(plane)
 
-    for f, (x, y) in annot_pred.iterrows():
-        print(x, y)
-        cv2.drawMarker(plane, (y, x), (255), markerType=cv2.MARKER_CROSS, markerSize=10)
+        for f, (x, y) in annot_image_pred.iterrows():
+            cv2.drawMarker(plane, (x, y), 255, markerType=cv2.MARKER_CROSS, markerSize=10)
 
-    for row in annot_actual.iterrows():
-        x, y = row.loc[[ 'x', 'y']]
-    print(0)
+        for f, row in annot_image_actual.iterrows():
+            x, y = row.loc[['x', 'y']]
+            logging.info(f"{x}, {y}")
+            cv2.drawMarker(plane, (x, y), 255, markerType=cv2.MARKER_SQUARE, markerSize=10)
+
+        tf.imwrite(path_pred_vis / (name_file + '_annot.png'), plane)
 
 
 if __name__ == '__main__':
