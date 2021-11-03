e8c91773de114b12e8727999c3e016a44ed2cef9
Leo Burgy <leo.burgy@gmail.com>
2021-11-03 14:49:05 +0100

    Add syntethic procentrioles

--- metrics.py
+++ metrics.py
@@ -11,12 +11,17 @@ rng = default_rng(1993)
 def main():
     height, width = 2048, 2048
     object_number = 20
+    has_daughter = .3
     noise_gaussian_scale = 1
     false_positives_n = int(.1 * object_number)
     false_negatives_n = int(.1 * object_number)
 
     # Generate ground truth objects (true positives)
     object_positions_actual = rng.integers(0, height, size=(object_number, 2))
+    procentrioles = rng.choice(object_positions_actual, int(has_daughter * object_number)) + 5
+    object_positions_actual = np.concatenate([object_positions_actual, procentrioles])
+
+    object_number, _ = object_positions_actual.shape
 
     # Simulate the predictions and delete some objects (the false negatives)
     noise = rng.normal(scale=noise_gaussian_scale, size=(object_number, 2)).astype(int)
