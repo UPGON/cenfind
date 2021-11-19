8a915d938aebdcc1efc6b4a233b05006a03e84ec
Leo Burgy <leo.burgy@epfl.ch>
2021-11-19 14:09:59 +0100

    Reformat test data

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -4,22 +4,8 @@ from centrack.metrics import compute_metrics
 
 
 def test_compute_metrics():
-    positions = np.array([
-        [2, 2],
-        [2, 5],
-        [2, 7],
-        [6, 2],
-        [6, 5],
-        [8, 2]
-    ])
-
-    predictions = np.array([
-        [2, 2],
-        [2, 6],
-        [5, 3],
-        [8, 2],
-        [8, 8]
-    ])
+    positions = np.array([[2, 2], [2, 5], [2, 7], [6, 2], [6, 5], [8, 2]])
+    predictions = np.array([[2, 2], [2, 6], [5, 3], [8, 2], [8, 8]])
 
     result = compute_metrics(positions, predictions, offset_max=2)
 
