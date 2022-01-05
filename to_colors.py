a105e9da3ab1e55655bd3fa5e0c9f8e60c0fed19
Leo Burgy <leo.burgy@epfl.ch>
2022-01-05 07:37:09 +0100

    Invert output images

--- to_colors.py
+++ to_colors.py
@@ -1,10 +1,13 @@
 from pathlib import Path
+
+import cv2
+
 from centrack.utils import channels_combine
 import tifffile as tf
 
 
 def main():
-    path_dataset = Path('/Volumes/work/epfl/datasets/RPE1wt_CP110+GTU88+PCNT_2')
+    path_dataset = Path('/Volumes/work/epfl/datasets/RPE1wt_CEP152+GTU88+PCNT_1')
     path_projections = path_dataset / 'projections'
     path_color = (path_dataset / 'color')
     path_color.mkdir(exist_ok=True)
@@ -13,6 +16,7 @@ def main():
         if path.name.endswith('tif') and not path.name.startswith('.'):
             data = tf.imread(path)
             color = channels_combine(data, (1, 2, 3))
+            color = cv2.bitwise_not(color)
             stem = path.name.removesuffix(''.join(path.suffixes))
             tf.imwrite(path_color / f'{stem}_color.png', color)
 
