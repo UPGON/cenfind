b99f7bd8643ea2ce805cb252a8057c5a59123979
Leo Burgy <leo.burgy@epfl.ch>
2022-01-27 16:42:47 +0100

    Refactor the order of the calls in upload_mal.py

--- scripts/upload_mal.py
+++ scripts/upload_mal.py
@@ -2,7 +2,6 @@ import logging
 import uuid
 
 from labelbox import Client, LabelingFrontend
-from labelbox.schema.ontology import OntologyBuilder, Tool
 
 from centrack.utils import get_lb_api_key
 
@@ -14,23 +13,20 @@ def main():
     client = Client(api_key=lb_api_key)
     logging.info('Connection established')
 
-    # Only update this if you have an on-prem deployment
-    ontology_builder = OntologyBuilder(tools=[
-        Tool(tool=Tool.Type.POINT, name="centriole"),
-    ])
-    project = client.create_project(name="image_mal_project")
-    dataset = client.create_dataset(name="image_mal_dataset", iam_integration=None)
-    test_img_url = "https://raw.githubusercontent.com/Labelbox/labelbox-python/develop/examples/assets/2560px-Kitano_Street_Kobe01s5s4110.jpg"
-    data_row = dataset.create_data_row(row_data=test_img_url)
-    editor = next(
-        client.get_labeling_frontends(where=LabelingFrontend.name == 'editor'))
-    project.setup(editor, ontology_builder.asdict())
-    project.datasets.connect(dataset)
+    project = client.get_project('cktegl25f5d4v0yc47b5n4gtx')
     project.enable_model_assisted_labeling()
-    ontology = ontology_builder.from_project(project)
-    feature_schema_id = ontology.tools[0].feature_schema_id
 
-    # For more details see image_mal.ipynb or ner_mal.ipynb
+    ontology = client.get_ontology('ckywqubua5nkp0zb2h9lm3vn7')
+    editor = next(client.get_labeling_frontends(where=LabelingFrontend.name == 'editor'))
+    # project.setup(editor, ontology)
+
+    dataset = client.create_dataset(name="Test", iam_integration=None)
+    project.datasets.connect(dataset)
+
+    test_img_path = '/Volumes/work/epfl/datasets/20210727_HA-FL-SAS6_Clones/projections_channel/CPAP/png/20210727_RPE1_HA-FL_S6_CloneE7_Ref_DAPI+rPOC5AF488+mHA568+gCPAP647_R1_1_MMStack_Default_max_C3.png'
+    data_row = dataset.create_data_row(row_data=test_img_path)
+    feature_schema_id = ontology.tools()[3].feature_schema_id
+
     annotations = [{
         "uuid": str(uuid.uuid4()),
         "schemaId": feature_schema_id,
@@ -43,7 +39,7 @@ def main():
         }
     }]
 
-    upload_job = project.upload_annotations(name="upload_py_object_job",
+    upload_job = project.upload_annotations(name="upload_predictions",
                                             annotations=annotations)
     upload_job.wait_until_done()
 
