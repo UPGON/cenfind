4becb72c4c16323bffced78363a158cdc979cc81
Leo Burgy <leo.burgy@epfl.ch>
2022-02-04 09:46:43 +0100

    Refactor upload_mal.py`

--- scripts/upload_mal.py
+++ scripts/upload_mal.py
@@ -1,7 +1,8 @@
 import logging
 import uuid
-from cv2 import cv2
+
 import numpy as np
+from cv2 import cv2
 from labelbox import (
     Client, OntologyBuilder, LabelingFrontend, Dataset, Project
     )
@@ -18,13 +19,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.DEBUG)
 
 
-def main():
-    with open('labelbox_api_key.txt', 'r') as apikey:
-        lb_api_key = apikey.readline().rstrip('\n')
-
-    client = Client(api_key=lb_api_key)
-
-    project_name = 'Test project'
+def project_create(client, project_name):
     projects_test = client.get_projects(where=Project.name == project_name)
 
     try:
@@ -35,36 +30,54 @@ def main():
         logger.debug('No such test project; creating...')
     finally:
         project = client.create_project(name=project_name, description='')
+    return project
 
-    project.enable_model_assisted_labeling()
-    logger.debug('MAL enabled.')
 
-    logger.debug('Getting the ontology')
-    ontology = client.get_ontology(ontology_id='ckywqubua5nkp0zb2h9lm3vn7')
+def ontology_setup(client, project, ontology_id):
+    """
+    Fetch the ontology using the ID and attach it to the project.
+    This is in place.
+    :param client:
+    :param project:
+    :param ontology_id:
+    :return:
+    """
+    ontology = client.get_ontology(ontology_id=ontology_id)
     ontology_builder = OntologyBuilder.from_ontology(ontology)
     editor = next(
         client.get_labeling_frontends(where=LabelingFrontend.name == 'editor'))
     project.setup(editor, ontology_builder.asdict())
 
-    dataset_name = 'Test dataset'
+
+def dataset_create(client, dataset_name):
+    """
+    Create a dataset object and delete any dataset with same name.
+    :param client:
+    :param dataset_name:
+    :return:
+    """
     datasets_test = client.get_datasets(where=Dataset.name == dataset_name)
     try:
         dataset = next(datasets_test)
-        logger.debug('Found a test dataset (%s)', dataset)
+        logger.debug('Found a test dataset (%s)', dataset.name)
         dataset.delete()
     except StopIteration:
         logger.debug('No such test dataset; creating one...')
     finally:
         dataset = client.create_dataset(name=dataset_name, iam_integration=None)
+    return dataset
 
-    project.datasets.connect(dataset)
-    logger.debug('Attaching the dataset to the project.')
 
-    canvas = np.zeros((2048, 2048), 'uint8')
+def labels_list_create(shape, number_foci):
+    """
+    Generate a synthetic annotated dataset and convert it as a LabelList object.
+    :return:
+    """
+    canvas = np.zeros(shape, 'uint8')
     annotations = []
-    random_2dpoints = np.random.randint(0, 2048, (10, 2))
+    random_2dpoints = np.random.randint(0, min(shape), (number_foci, 2))
     for r, c in random_2dpoints:
-        logger.debug('adding annot')
+        logger.debug('Generate object at position %s', (r, c))
         cv2.circle(canvas, (r, c), 20, 255, thickness=cv2.FILLED)
         annot = ObjectAnnotation(name='Centriole', value=Point(x=r, y=c))
         annotations.append(annot)
@@ -73,6 +86,10 @@ def main():
     labels_list = LabelList()
     labels_list.append(Label(data=image_data, annotations=annotations))
 
+    return labels_list
+
+
+def prepare_upload_task(client, project, dataset, labels_list):
     signer = lambda _bytes: client.upload_data(content=_bytes, sign=True)
 
     labels_list.assign_feature_schema_ids(OntologyBuilder.from_project(project))
@@ -84,6 +101,33 @@ def main():
         annotations=ndjsons,
         validate=True
         )
+    return task
+
+
+def main():
+    with open('../configs/labelbox_api_key.txt', 'r') as apikey:
+        lb_api_key = apikey.readline().rstrip('\n')
+
+    client = Client(api_key=lb_api_key)
+
+    project_name = 'Test project.'
+    project = project_create(client, project_name)
+
+    logger.debug('Enable MAL.')
+    project.enable_model_assisted_labeling()
+
+    logger.debug('Get the ontology.')
+    ontology_setup(client, project, ontology_id='ckywqubua5nkp0zb2h9lm3vn7')
+
+    dataset_name = 'Test dataset.'
+    dataset = dataset_create(client, dataset_name)
+
+    project.datasets.connect(dataset)
+    logger.debug('Attach the dataset to the project.')
+
+    labels_list = labels_list_create((2048, 2048), 10)
+
+    task = prepare_upload_task(client, project, dataset, labels_list)
     task.wait_until_done()
 
 
