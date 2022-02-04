0972c3297ae8e5f49a2316d2bd4d5b74fcc37485
Leo Burgy <leo.burgy@epfl.ch>
2022-02-04 12:33:37 +0100

    Move labelbox api function to their module.

--- scripts/labelbox_upload_mal.py
+++ scripts/labelbox_upload_mal.py
@@ -1,27 +1,20 @@
 import logging
-import uuid
 from pathlib import Path
 
 import numpy as np
 import tifffile as tf
-from cv2 import cv2
-from labelbox import (
-    Client,
-    OntologyBuilder,
-    LabelingFrontend,
-    Dataset,
-    Project,
-    )
-from labelbox.data.annotation_types import (
-    ObjectAnnotation,
-    Point,
-    LabelList,
-    Label,
-    ImageData,
-    )
-from labelbox.data.serialization import NDJsonConverter
+from labelbox import Client
 
 from centrack.data import DataSet
+from centrack.labelbox_api import (
+    project_create,
+    dataset_create,
+    ontology_setup,
+    generate_image,
+    create_label,
+    labels_list_create,
+    prepare_upload_task
+    )
 from centrack.utils import contrast, extract_centriole
 
 logger = logging.getLogger(__name__)
@@ -29,123 +22,6 @@ logger.setLevel(logging.DEBUG)
 
 synthetic = False
 dataset_name = '20210727_HA-FL-SAS6_Clones'
-
-
-def project_create(client, project_name):
-    """
-    Create a project object and delete any existing with `project name`.
-    :param client:
-    :param project_name:
-    :return:
-    """
-    projects_test = client.get_projects(where=Project.name == project_name)
-
-    try:
-        project = next(projects_test)
-        logger.debug('Project test found (%s)', project.uid)
-        project.delete()
-    except StopIteration:
-        logger.debug('No such test project; creating...')
-    finally:
-        project = client.create_project(name=project_name, description='')
-    return project
-
-
-def ontology_setup(client, project, ontology_id):
-    """
-    Fetch the ontology using the ID and attach it to the project.
-    This is in place.
-    :param client:
-    :param project:
-    :param ontology_id:
-    :return:
-    """
-    ontology = client.get_ontology(ontology_id=ontology_id)
-    ontology_builder = OntologyBuilder.from_ontology(ontology)
-    editor = next(
-        client.get_labeling_frontends(where=LabelingFrontend.name == 'editor'))
-    project.setup(editor, ontology_builder.asdict())
-
-
-def dataset_create(client, dataset_name):
-    """
-    Create a dataset object and delete any dataset with same name.
-    :param client:
-    :param dataset_name:
-    :return:
-    """
-    datasets_test = client.get_datasets(where=Dataset.name == dataset_name)
-    try:
-        dataset = next(datasets_test)
-        logger.debug('Found a test dataset (%s)', dataset.name)
-        dataset.delete()
-    except StopIteration:
-        logger.debug('No such test dataset; creating one...')
-    finally:
-        dataset = client.create_dataset(name=dataset_name, iam_integration=None)
-    return dataset
-
-
-def generate_image(canvas, predictions):
-    """
-    Draw foci on a black canvas at positions of predictions.
-    :param canvas:
-    :param predictions:
-    :return:
-    """
-    for r, c in predictions:
-        cv2.circle(canvas, (r, c), 20, 255, thickness=cv2.FILLED)
-    return canvas
-
-
-def to_labelbox_format(predictions):
-    """
-    Convert numpy coordinates into Labelbox format.
-    :param predictions:
-    :return:
-    """
-    annotations = []
-    for r, c in predictions:
-        annot = ObjectAnnotation(name='Centriole', value=Point(x=r, y=c))
-        annotations.append(annot)
-    return annotations
-
-
-def create_label(image, predictions):
-    """
-    Combine an image and its annotation into a Label.
-    :param predictions:
-    :param image:
-    :return:
-    """
-    return Label(data=ImageData.from_2D_arr(image),
-                 annotations=to_labelbox_format(predictions))
-
-
-def labels_list_create(labels):
-    """
-    Combine all labels into a LabelList object.
-    :return:
-    """
-    labels_list = LabelList()
-    for label in labels:
-        labels_list.append(label)
-    return labels_list
-
-
-def prepare_upload_task(client, project, dataset, labels_list):
-    signer = lambda _bytes: client.upload_data(content=_bytes, sign=True)
-
-    labels_list.assign_feature_schema_ids(OntologyBuilder.from_project(project))
-    labels_list.add_to_dataset(dataset, signer)
-
-    ndjsons = list(NDJsonConverter.serialize(labels_list))
-    task = project.upload_annotations(
-        name=f'upload-job-{uuid.uuid4()}',
-        annotations=ndjsons,
-        validate=True
-        )
-    return task
 
 
 def main():
