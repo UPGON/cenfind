4d155b774b3cbf9684acd36e970b3ccaca72e0d7
Leo Burgy <leo.burgy@epfl.ch>
2022-02-04 09:15:19 +0100

    Replace upload_mal.py with the script version

--- scripts/upload_mal.py
+++ scripts/upload_mal.py
@@ -1,195 +1,90 @@
 import logging
-import logging
-import shutil
 import uuid
-from pathlib import Path
-from random import randint
-
-import tifffile as tf
-import labelbox
-import numpy as np
 from cv2 import cv2
-from labelbox import OntologyBuilder, LabelingFrontend, Client
+import numpy as np
+from labelbox import (
+    Client, OntologyBuilder, LabelingFrontend, Dataset, Project
+    )
 from labelbox.data.annotation_types import (
-    ImageData,
     ObjectAnnotation,
-    Label,
-    LabelList,
     Point,
+    LabelList,
+    Label,
+    ImageData,
     )
 from labelbox.data.serialization import NDJsonConverter
 
-from centrack.annotation import Centre
-from centrack.data import Field, DataSet
-from centrack.labelbox_api import get_dataset_uid, get_project_uid, \
-    get_lb_api_key
-
-logging.basicConfig(level=logging.DEBUG)
-
-
-def create_image(foci_number=1, brightness=255, width=2048, height=2048):
-    canvas = np.zeros((width, height), 'uint8')
-    annotation = []
-    for _ in range(foci_number):
-        x, y = randint(0, width - 1), randint(0, height - 1)
-        annotation.append((x, y))
-        logging.debug('Generate a point at %s', (x, y))
-        cv2.circle(canvas, (x, y), 2, brightness, cv2.FILLED)
-    return annotation, canvas
-
-
-def format_annotations(centres_list):
-    annotations = []
-    for centre in centres_list:
-        name = 'Centriole'
-        r, c = centre.position
-        logging.debug('Add the point %s', (r, c))
-        annotations.append(ObjectAnnotation(
-            name=name,
-            value=Point(x=r, y=c)
-            ))
-
-    return annotations
-
-
-def create_project(project_name, client):
-    project_uid = get_project_uid(client, project_name)
-
-    if project_uid:
-        project = client.get_project(project_uid)
-        logging.info('Project (%s) already exists.', project_name)
-    else:
-        project = client.create_project(name=project_name,
-                                        description="")
-        project.enable_model_assisted_labeling()
-        add_ontology(ontology_id='ckywqubua5nkp0zb2h9lm3vn7',
-                     project=project,
-                     client=client)
-        logging.info('Project (%s) created.', project_name)
-
-    return project
-
-
-def create_dataset(dataset_name, client):
-    dataset_uid = get_dataset_uid(client, dataset_name)
-
-    if dataset_uid:
-        dataset = client.get_dataset(dataset_uid)
-        logging.info(f'Dataset (%s) already exists. It will be deleted.',
-                     dataset_name)
-        dataset.delete()
-    dataset = client.create_dataset(name=dataset_name,
-                                    iam_integration=None)
-    logging.info(f'Dataset (%s) created.', dataset_name)
-
-    return dataset
-
-
-def add_ontology(ontology_id, project, client):
-    ontology = client.get_ontology(ontology_id)
-    ontology_builder = OntologyBuilder.from_ontology(ontology)
-    editor = next(
-        client.get_labeling_frontends(
-            where=LabelingFrontend.name == 'editor'))
-
-    project.setup(editor, ontology_builder.asdict())
-
-
-def annotation_data_upload(client, project, dataset, labels_list):
-    ontology_builder = OntologyBuilder.from_project(project)
-    signer = lambda _bytes: client.upload_data(content=_bytes, sign=True)
-
-    (labels_list.assign_feature_schema_ids(ontology_builder)
-     .add_to_dataset(dataset, signer))
-
-    ndjsons = list(NDJsonConverter.serialize(labels_list))
-    upload_task = project.upload_annotations(
-        name=f"upload-job-{uuid.uuid4()}",
-        annotations=ndjsons,
-        validate=True)
-
-    return upload_task
-
-
-def label_list_from(mapping_dataset):
-    labels_list = LabelList()
-    for path, (annotation, foci) in mapping_dataset.items():
-        foci = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-        image_data = ImageData.from_2D_arr(foci)
-        annotations_gt = [Centre(position) for position in annotation]
-        annotations = format_annotations(annotations_gt)
-        labels_list.append(Label(
-            data=image_data,
-            annotations=annotations
-            ))
-    return labels_list
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.DEBUG)
 
 
 def main():
+    with open('labelbox_api_key.txt', 'r') as apikey:
+        lb_api_key = apikey.readline().rstrip('\n')
 
-    synthetic = False
-
-    if synthetic:
-        image_path = Path('../out/mal_upload')
-
-        if image_path.exists():
-            shutil.rmtree(image_path)
-            logging.warning('Delete the existing folder of synthetic images.')
-
-        image_path.mkdir()
-
-        logging.debug('Create synthetic images')
-        mapping_dataset = dict()
-        for i in range(3):
-            path_dst = str(image_path / f'image{i:02}.png')
-            mapping_dataset[path_dst] = create_image(foci_number=10)
-
-        logging.debug('Save synthetic images')
-        for path, (annotation, image) in mapping_dataset.items():
-            cv2.imwrite(path, image)
-    else:
-        mapping_dataset = dict()
-        dataset_path = Path('/Volumes/work/epfl/datasets/20210727_HA-FL-SAS6_Clones/projections')
-        dataset = DataSet(dataset_path)
-        for path in dataset.projections.iterdir():
-            if path.name.startswith('.tif'):
-                image = tf.imread(path)
-
-
-    lb_api_key = get_lb_api_key('../configs/labelbox_api_key.txt')
     client = Client(api_key=lb_api_key)
-    logging.info('Connection established')
 
-    project_name = 'Test Project 4'
-    project = create_project(project_name, client)
+    project_name = 'Test project'
+    projects_test = client.get_projects(where=Project.name == project_name)
 
-    dataset_name = 'Test Dataset'
-    dataset = create_dataset(dataset_name,
-                             client)  # Error upon second run when the project already exists
+    try:
+        project = next(projects_test)
+        logger.debug('Project test found (%s)', project)
+        project.delete()
+    except StopIteration:
+        logger.debug('No such test project; creating...')
+    finally:
+        project = client.create_project(name=project_name, description='')
+
+    project.enable_model_assisted_labeling()
+    logger.debug('MAL enabled.')
+
+    logger.debug('Getting the ontology')
+    ontology = client.get_ontology(ontology_id='ckywqubua5nkp0zb2h9lm3vn7')
+    ontology_builder = OntologyBuilder.from_ontology(ontology)
+    editor = next(
+        client.get_labeling_frontends(where=LabelingFrontend.name == 'editor'))
+    project.setup(editor, ontology_builder.asdict())
+
+    dataset_name = 'Test dataset'
+    datasets_test = client.get_datasets(where=Dataset.name == dataset_name)
+    try:
+        dataset = next(datasets_test)
+        logger.debug('Found a test dataset (%s)', dataset)
+        dataset.delete()
+    except StopIteration:
+        logger.debug('No such test dataset; creating one...')
+    finally:
+        dataset = client.create_dataset(name=dataset_name, iam_integration=None)
 
     project.datasets.connect(dataset)
+    logger.debug('Attaching the dataset to the project.')
 
-    is_mal = False
+    canvas = np.zeros((2048, 2048), 'uint8')
+    annotations = []
+    random_2dpoints = np.random.randint(0, 2048, (10, 2))
+    for r, c in random_2dpoints:
+        logger.debug('adding annot')
+        cv2.circle(canvas, (r, c), 20, 255, thickness=cv2.FILLED)
+        annot = ObjectAnnotation(name='Centriole', value=Point(x=r, y=c))
+        annotations.append(annot)
 
-    if is_mal:
-        labels_list = label_list_from(mapping_dataset)
-        upload_task = annotation_data_upload(client, project, dataset, labels_list)
-        upload_task.wait_until_done()
-    else:
-        datarows = [p for p in mapping_dataset.keys()]
-        uploads = []
-        for path in datarows:
-            path_name = Path(path).name
-            item = {labelbox.DataRow.row_data: path,
-                    labelbox.DataRow.external_id: path_name}
-            uploads.append(item)
+    image_data = ImageData.from_2D_arr(canvas)
+    labels_list = LabelList()
+    labels_list.append(Label(data=image_data, annotations=annotations))
 
-        upload_task = dataset.create_data_rows(uploads)
+    signer = lambda _bytes: client.upload_data(content=_bytes, sign=True)
 
-    upload_task.wait_till_done()
+    labels_list.assign_feature_schema_ids(OntologyBuilder.from_project(project))
+    labels_list.add_to_dataset(dataset, signer)
 
-
-    logging.info('Dataset has been attached to the project')
+    ndjsons = list(NDJsonConverter.serialize(labels_list))
+    task = project.upload_annotations(
+        name=f'upload-job-{uuid.uuid4()}',
+        annotations=ndjsons,
+        validate=True
+        )
+    task.wait_until_done()
 
 
 if __name__ == '__main__':
