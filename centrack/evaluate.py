9e121ffeb581175baa42bc630f870075065f69a7
Leo Burgy <leo.burgy@epfl.ch>
2022-02-09 11:06:18 +0100

    Update docstring

--- centrack/evaluate.py
+++ centrack/evaluate.py
@@ -12,41 +12,49 @@ from centrack.score import extract_centrioles
 
 logging.basicConfig(level=logging.INFO)
 
-CYAN_RGB = (0, 255, 255)
-RED_RGB = (255, 0, 0)
-WHITE_RGB = (255, 255, 255)
 rng = default_rng(1993)
 
 
 def generate_synthetic_data(height=512, size=200, has_daughter=.8):
-    # Generate ground truth objects (true positives)
+    """
+    Generate ground truth objects (true positives)
+    :param height:
+    :param size:
+    :param has_daughter:
+    :return:
+    """
     foci = rng.integers(0, height, size=(size, 2))
     daughter_n = int(has_daughter * size)
     offset = rng.integers(-4, 4, size=(daughter_n, 2))
 
     daughters = rng.choice(foci, daughter_n, replace=False) + offset
-    foci = np.concatenate([foci, daughters])
+    foci = np.concatenate([foci, daughters], axis=0)
 
-    return foci
+    return [Centre(f) for f in foci]
 
 
-def generate_predictions(height, foci, fn_rate=.1, fp_rate=.2, random=False):
+def generate_predictions(height: int, foci, fn_rate=.1, fp_rate=.2,
+                         random=False):
+    """
+    Generate predictions from annotation
+    :param height:
+    :param foci:
+    :param fn_rate:
+    :param fp_rate:
+    :param random:
+    :return: List[Centre]
+    """
     size = len(foci)
 
     fp_n = int(fp_rate * size)
     fn_n = int(fn_rate * size)
 
-    if random:
-        predictions = rng.integers(0, height, size=(50, 2))
-        return predictions
-    else:
-        # Simulate the predictions and delete some objects (the false negatives)
-        predictions = foci.copy()
-        predictions = rng.choice(predictions, size - fn_n, replace=False)
-        fps = rng.integers(0, height, size=(fp_n, 2))
-        predictions = np.concatenate([predictions, fps], axis=0)
+    predictions = foci.copy()
+    predictions = rng.choice(predictions, size - fn_n, replace=False)
+    fps = rng.integers(0, height, size=(fp_n, 2))
+    predictions = np.concatenate([predictions, fps], axis=0)
 
-        return predictions
+    return [Centre(f) for f in predictions]
 
 
 def compute_metrics(positions, predictions, offset_max=2):
@@ -112,8 +120,8 @@ def precision_recall(metrics):
 def process_one_image(data, annotation):
     predictions = extract_centrioles(data)
     predictions_np = np.asarray([c.to_numpy() for c in predictions])
-    positions_np = predictions_np
-    confusion_matrix = compute_metrics(positions_np, predictions_np,
+    annotation_np = np.asarray([a.to_numpy() for a in annotation])
+    confusion_matrix = compute_metrics(annotation_np, predictions_np,
                                        offset_max=2)
 
     return precision_recall(confusion_matrix)
