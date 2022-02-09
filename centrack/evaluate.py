0e77e4cde5a4f17f4cdaafc8424b105acce68d07
Leo Burgy <leo.burgy@epfl.ch>
2022-02-09 09:08:38 +0100

    Delete unused functions

--- centrack/evaluate.py
+++ centrack/evaluate.py
@@ -1,52 +1,9 @@
 import numpy as np
-from cv2 import cv2
 from numpy.random import default_rng
 from scipy.optimize import linear_sum_assignment
 from scipy.spatial.distance import cdist
 
-from centrack.outline import Centre, Contour
-
 rng = default_rng(1993)
-
-
-def mask_from(annotation, w=2048, h=2048, radius=2):
-    """Draw a mask of the objects (points, contours).
-    This function computes quantitative masks not a visualisation
-    """
-    mask = np.zeros((w, h), dtype=np.uint8)
-
-    for element in annotation:
-        if isinstance(element, Centre):
-            cv2.circle(mask, element.centre, radius, 1, thickness=-1)
-
-        if isinstance(element, Contour):
-            cv2.drawContours(mask, [element], 0, 1, thickness=-1)
-
-    return mask
-
-
-def overlap(mask_actual, mask_pred):
-    """Make a visual estimation of the overlap of two masks."""
-    if mask_actual.shape != mask_pred.shape:
-        raise ValueError(
-            f'mask_actual shape ({mask_actual.shape})!= mask_pred shape ({mask_pred.shape})')
-
-    h, w = mask_actual.shape
-
-    comparison = np.zeros((h, w, 3), dtype=np.uint8)
-    comparison[:, :, 0] = mask_pred
-    comparison[:, :, 1] = mask_actual
-
-    return comparison
-
-
-def iou(mask_pred, mask_actual, w, h, radius):
-    """Compute the intersection over union of two masks."""
-    mask_and = np.logical_and(mask_actual, mask_pred)
-    mask_or = np.logical_or(mask_actual, mask_pred)
-    iou = ((mask_and.sum() + 1e-5) / (mask_or.sum() + 1e-5)).round(3)
-
-    return iou
 
 
 def generate_synthetic_data(height=512, size=200, has_daughter=.8):
