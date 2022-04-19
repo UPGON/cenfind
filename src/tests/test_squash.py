fb66ce2a3b7213a9bdb53ce1353d53ba3c7cb8d7
Leo Burgy <leo.burgy@gmail.com>
2022-04-19 15:18:03 +0200

    Use fixture for stacks and projection

--- src/tests/test_squash.py
+++ src/tests/test_squash.py
@@ -1,13 +1,24 @@
 import numpy as np
 from pathlib import Path
+
+import pytest
+
 from centrack.commands.squash import squash, read_ome_tif
+
+@pytest.fixture()
+def empty_stack():
+    return np.zeros((4, 67, 2048, 2048), dtype='uint16')
+
+@pytest.fixture()
+def projection():
+    return np.zeros((4, 2048, 2048), dtype='uint16')
 
 def test_read_ome_tif():
     path = Path('../../data/RPE1wt_CEP152+GTU88+PCNT_1/raw/RPE1wt_CEP152+GTU88+PCNT_1_MMStack_1-Pos_000_000.ome.tif')
     data = read_ome_tif(path)
     assert data.shape == (4, 67, 2048, 2048)
 
-def test_squash():
-    data = np.ndarray((4, 62, 2048, 2048))
+def test_squash(empty_stack, projection):
+    data = empty_stack
     projected = squash(data)
-    assert projected.shape == (4, 2048, 2048)
+    assert projection.shape == projected.shape
