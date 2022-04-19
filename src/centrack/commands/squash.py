b54d04510c1d56737eba00c0102c54a35cf21122
Leo Burgy <leo.burgy@gmail.com>
2022-04-19 15:08:39 +0200

    Write ome_tif_read function

--- src/centrack/commands/squash.py
+++ src/centrack/commands/squash.py
@@ -1,5 +1,20 @@
 import numpy
+import tifffile as tf
+from pathlib import Path
 
+def read_ome_tif(path: Path):
+    """
+    Read an OME tif file from the disk into a numpy array
+    :param path:
+    :return:
+    """
+    data = tf.imread(path)
+    return data
 
 def squash(data: numpy.ndarray):
+    """
+    Squash a numpy array along the z-axis using max function.
+    :param data:
+    :return:
+    """
     return data.max(axis=1)
--- src/tests/test_squash.py
+++ src/tests/test_squash.py
@@ -1,6 +1,11 @@
-import pytest
 import numpy as np
-from centrack.commands.squash import squash
+from pathlib import Path
+from centrack.commands.squash import squash, read_ome_tif
+
+def test_read_ome_tif():
+    path = Path('../../data/RPE1wt_CEP152+GTU88+PCNT_1/raw/RPE1wt_CEP152+GTU88+PCNT_1_MMStack_1-Pos_000_000.ome.tif')
+    data = read_ome_tif(path)
+    assert data.shape == (4, 67, 2048, 2048)
 
 def test_squash():
     data = np.ndarray((4, 62, 2048, 2048))
