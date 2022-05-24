4ece48740091f77ecbc7e3b71f31636c0e545312
Leo Burgy <leo.burgy@gmail.com>
2022-05-24 11:34:51 +0200

    Update

--- src/centrack/misc/train_stardist.py
+++ src/centrack/misc/train_stardist.py
@@ -1,5 +1,13 @@
+from stardist.models import Config2D, StarDist2D, StarDistData2D
+from glob import glob
+
+
 def main():
-    ...
+    images = sorted(glob('/data1/centrioles/rpe/projections/*_max_C2.tif'))
+    masks = sorted(glob('/data1/centrioles/rpe/projections/*_max_C2.tif'))
+
+    print(images)
+    print(masks)
 
 
 if __name__ == '__main__':
