cb1a8e0b57811c8c0e950cec7e891f1a940620cb
Leo Burgy <leo.burgy@gmail.com>
2022-07-20 11:46:14 +0200

    Clean train.py

--- src/centrack/commands/train.py
+++ src/centrack/commands/train.py
@@ -2,10 +2,7 @@ from pathlib import Path
 import json
 import numpy as np
 import tifffile as tif
-import tensorflow as tf
-from albumentations import Compose, RandomBrightnessContrast, HorizontalFlip, VerticalFlip, Rotate
 
-from centrack.utils.visualisation import show_images
 from spotipy.spotipy.utils import points_to_prob, normalize_fast2d
 from spotipy.spotipy.model import SpotNet, Config
 
@@ -18,8 +15,10 @@ def load_pairs(path, split='train'):
     """
     path_projections = path / 'projections'
     path_centrioles = path / 'annotations/centrioles'
+
     with open(path / f'{split}_channels.txt', 'r') as fh:
         fovs = [i.strip().split(',') for i in fh.readlines()]
+
     images = []
     positions = []
     for fov, chid in fovs:
@@ -27,45 +26,27 @@ def load_pairs(path, split='train'):
         image_path = str(path_projections / f"{fov}_max.tif")
         image = tif.imread(image_path)
         image = image[chid, :, :]
-        imaged_norm = normalize_fast2d(image)
-        images.append(imaged_norm)
+        image = normalize_fast2d(image)
+        images.append(image)
 
         foci_path = str(path_centrioles / f"{fov}_max_C{chid}.txt")
-        foci = np.loadtxt(foci_path, dtype=int, delimiter=',')
+        foci = np.loadtxt(foci_path, dtype=int, delimiter=',')  # in format x, y; origin at top left
 
-        foci_mask = points_to_prob(foci, shape=fov_shape, sigma=1)
+        foci_mask = points_to_prob(foci, shape=image.shape, sigma=1)
         positions.append(foci_mask)
 
-    images = np.stack(images)
-    positions = np.stack(positions)
-    print(images.shape)
+    return np.stack(images), np.stack(positions)
 
-    return images, positions
-
-
-transform = Compose([Rotate(p=.5),
-                     RandomBrightnessContrast(p=.5),
-                     HorizontalFlip(p=.5),
-                     VerticalFlip(p=.5)
-                     ])
 
 if __name__ == '__main__':
-    # Load the data...
     path_dataset = Path('/Users/buergy/Dropbox/epfl/datasets/RPE1wt_CEP63+CETN2+PCNT_1')
-
-    fov_shape = (2048, 2048)
-
     train_x, train_y = load_pairs(path_dataset, split='train')
     test_x, test_y = load_pairs(path_dataset, split='test')
 
-    # Load the config
-    with open('../../../models/leo3_multiscale_True_mae_aug_1_sigma_1.5_split_2_batch_2_n_300/config.json',
+    with open('../../../models/config.json',
               'r') as config:
         config_dict = json.load(config)
     config = Config(**config_dict)
 
-    # Create a model instance
     model = SpotNet(config, name=None, basedir='/Users/buergy/Dropbox/epfl/projects/centrack/models/test_model')
-
-    # Train loop
     model.train(train_x, train_y, validation_data=(test_x, test_y))
