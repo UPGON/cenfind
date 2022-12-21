74c3a4eadbe67f18b639ddf6af8421f104524b15
Leo Burgy <leo.burgy@gmail.com>
2022-12-21 16:56:22 +0100

    Change channel range for vignettes.py

--- src/cenfind/labelbox/vignettes.py
+++ src/cenfind/labelbox/vignettes.py
@@ -12,11 +12,11 @@ def get_args():
     parser = argparse.ArgumentParser(description="VIGNETTE: create png version of channel+nuclei for annotation tool")
     parser.add_argument('path', type=Path, help="the path to the dataset")
     parser.add_argument('nuclei_index', type=int, help="the index of the nuclei (often, 0 or 3, first or last)")
+    parser.add_argument('centrioles_index', type=int, nargs='+', help="the index of the channels")
     parser.add_argument('--projection_suffix',
                         type=str,
                         default='_max',
                         help='the suffix indicating projection, e.g., `_max` or `_Projected`')
-    # parser.add_argument('--channel_index', type=int, help="the index of the channel (often, 1)")
     return parser.parse_args()
 
 
@@ -27,7 +27,7 @@ def main():
 
     pbar = tqdm(dataset.fields)
     for field in pbar:
-        for channel_id in range(1, 4):
+        for channel_id in args.centrioles_index:
             pbar.set_description(f"{field.name}: {channel_id}")
             vignette = create_vignette(field, channel_id, args.nuclei_index)
             dst = dataset.vignettes / f'{field.name}{args.projection_suffix}_C{channel_id}.png'
