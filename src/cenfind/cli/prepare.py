dedb6b17fb33359c749458c38d0d3a8e066e9f85
Leo Burgy <leo.burgy@gmail.com>
2022-12-21 16:13:15 +0100

    Remove logging

--- src/cenfind/cli/prepare.py
+++ src/cenfind/cli/prepare.py
@@ -1,5 +1,4 @@
 import argparse
-import logging
 import sys
 from pathlib import Path
 
@@ -27,40 +26,13 @@ def get_args():
     return args
 
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
-
-
 def main():
     args = get_args()
     path_dataset = args.path
     dataset = Dataset(path_dataset, projection_suffix=args.projection_suffix)
 
-    dataset.projections.mkdir(exist_ok=True)
-    logger.info('Projections folder created')
-
+    dataset.setup()
     dataset.write_fields()
-    logger.info('Field registry written')
-
-    dataset.predictions.mkdir(exist_ok=True)
-    (dataset.predictions / 'centrioles').mkdir(exist_ok=True)
-    (dataset.predictions / 'nuclei').mkdir(exist_ok=True)
-    logger.info('Prediction folder created')
-
-    dataset.statistics.mkdir(exist_ok=True)
-    logger.info('Statistics folder created')
-
-    dataset.visualisation.mkdir(exist_ok=True)
-    logger.info('Visualisation folder created')
-
-    dataset.vignettes.mkdir(exist_ok=True)
-    logger.info('Vignettes folder created')
 
 
 if __name__ == '__main__':
