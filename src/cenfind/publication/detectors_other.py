2ec8c2b2e44d9aad60c8a1f5005ff2a2f8279748
Leo Burgy <leo.burgy@epfl.ch>
2023-02-15 13:16:05 +0100

    Format

--- src/cenfind/publication/detectors_other.py
+++ src/cenfind/publication/detectors_other.py
@@ -17,7 +17,7 @@ def blob2point(keypoint: cv2.KeyPoint) -> tuple[int, ...]:
 def log_skimage(data: Field, channel: int, **kwargs) -> list:
     data = data.channel(channel)
     data = rescale_intensity(data, out_range=(0, 1))
-    foci = blob_log(data, min_sigma=1, max_sigma=2, num_sigma=10, threshold=.1)
+    foci = blob_log(data, min_sigma=1, max_sigma=2, num_sigma=10, threshold=0.1)
     res = [(int(r), int(c)) for r, c, _ in foci]
 
     return res
@@ -25,7 +25,7 @@ def log_skimage(data: Field, channel: int, **kwargs) -> list:
 
 def simpleblob_cv2(data: Field, channel: int, **kwargs) -> list:
     data = data.channel(channel)
-    foci = rescale_intensity(data, out_range='uint8')
+    foci = rescale_intensity(data, out_range="uint8")
     params = cv2.SimpleBlobDetector_Params()
 
     params.blobColor = 255
@@ -44,15 +44,18 @@ def simpleblob_cv2(data: Field, channel: int, **kwargs) -> list:
     return res
 
 
-def run_detection(method, data: Field,
-                  annotation: np.ndarray,
-                  tolerance,
-                  channel=None,
-                  model_path=None) -> Tuple[list[Centre], float]:
+def run_detection(
+    method,
+    data: Field,
+    annotation: np.ndarray,
+    tolerance,
+    channel=None,
+    model_path=None,
+) -> Tuple[list[Centre], float]:
     _foci = method(data, foci_model_file=model_path, channel=channel)
     if type(_foci) == tuple:
         prob_map, _foci = _foci
     res = points_matching(annotation, _foci, cutoff_distance=tolerance)
     f1 = np.round(res.f1, 3)
-    foci = [Centre((r, c), label='Centriole') for r, c in _foci]
+    foci = [Centre((r, c), label="Centriole") for r, c in _foci]
     return foci, f1
