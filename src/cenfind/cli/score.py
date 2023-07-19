d3e1dc3d2ce17657db8b08445bb776d72f64fe3d
Leo Burgy <leo.burgy@gmail.com>
2023-07-19 14:37:46 +0200

    Add logger

--- src/cenfind/cli/score.py
+++ src/cenfind/cli/score.py
@@ -4,13 +4,12 @@ from pathlib import Path
 
 import pandas as pd
 import tifffile as tif
-from tqdm import tqdm
-
 from cenfind.core.data import Dataset
 from cenfind.core.detectors import extract_foci, extract_nuclei
 from cenfind.core.log import get_logger
 from cenfind.core.measure import assign, save_foci, measure_signal_foci
 from cenfind.core.outline import visualisation
+from tqdm import tqdm
 
 
 def register_parser(parent_subparsers):
@@ -41,12 +40,7 @@ def register_parser(parent_subparsers):
         default=50,
         help="Distance threshold in pixel (default: 50 px)",
     )
-    parser.add_argument(
-        "--factor",
-        type=int,
-        default=256,
-        help="Factor to use: given a 2048x2048 image, 256 if 63x; 2048 if 20x.",
-    )
+
     parser.add_argument("--cpu", action="store_true", help="Only use the cpu")
 
     return parser
@@ -63,16 +57,10 @@ def run(args):
         os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
     dataset = Dataset(args.dataset)
+
     logger = get_logger(__name__, file=dataset.logs / 'cenfind.log')
 
-    if not any(dataset.projections.iterdir()):
-        logger.error(
-            "The projection folder (%s) is empty.\nPlease ensure you have run `squash` or that you have put the projections under projections/"
-            % dataset.projections, exc_info=True
-        )
-        raise FileNotFoundError
-
-    channels = dataset.fields[0].projection.shape[0]
+    channels = dataset.fields[0].data.shape[0]
 
     if args.channel_nuclei not in range(channels):
         logger.error("Index for nuclei (%s) out of index range" % args.channel_nuclei, exc_info=True)
@@ -96,26 +84,21 @@ def run(args):
         pbar.set_description(f"{field.name}")
         logger.info("Processing %s" % field.name)
 
+        nuclei = extract_nuclei(field, args.channel_nuclei)
+        if len(nuclei) == 0:
+            logger.warning("No nucleus has been detected in %s" % field.name)
+            continue
+
         for channel in args.channel_centrioles:
-            nuclei = extract_nuclei(field, args.channel_nuclei, args.factor)
-            if len(nuclei) == 0:
-                logger.warning("No nucleus has been detected in %s" % field.name)
-                continue
             logger.info("Processing %s / %d" % (field.name, channel))
-            foci = extract_foci(data=field, foci_model_file=args.model, channel=channel)
-            if len(foci) == 0:
-                logger.warning("No centrioles (channel: %s) has been detected in %s" % (channel, field.name))
+            foci = extract_foci(field=field, foci_model_file=args.model, channel=channel)
 
             nuclei_scored = assign(nuclei, foci, vicinity=args.vicinity)
             scored = score(field, nuclei_scored, channel)
             scores.append(scored)
 
-            predictions_path = (
-                    dataset.predictions
-                    / "centrioles"
-                    / f"{field.name}{dataset.projection_suffix}_C{channel}.txt"
-            )
-            measurements_path = dataset.measurements / f"{field.name}{dataset.projection_suffix}_C{channel}.txt"
+            predictions_path = dataset.predictions / "centrioles" / f"{field.name}_C{channel}.txt"
+            measurements_path = dataset.statistics / f"{field.name}_C{channel}.txt"
             measure_signal_foci(field, channel, foci, measurements_path)
             save_foci(foci, predictions_path)
 
@@ -133,10 +116,8 @@ def run(args):
                 % (field.name, channel, len(nuclei), len(foci))
             )
 
-            logger.info(
-                "Writing visualisations for (%s), channel %s" % (field.name, channel)
-            )
-            vis = visualisation(field, channel_centrioles=channel, channel_nuclei=args.channel_nuclei, nuclei=nuclei_scored)
+            vis = visualisation(field, channel_centrioles=channel, channel_nuclei=args.channel_nuclei,
+                                nuclei=nuclei_scored)
             tif.imwrite(
                 path_visualisation_model / f"{field.name}_C{channel}_pred.png", vis
             )
@@ -146,7 +127,7 @@ def run(args):
     flattened = [leaf for tree in scores for leaf in tree]
     scores_df = pd.DataFrame(flattened)
     scores_df.to_csv(dataset.statistics / "scores_df.tsv", sep="\t", index=False)
-    logger.info("Writing raw scores to %s" % str(dataset.statistics / "scores_df.tsv"))
+    logger.info("Writing raw scores to %s" % str(dataset.path / "statistics" / "scores_df.tsv"))
 
 
 if __name__ == "__main__":
@@ -156,5 +137,5 @@ if __name__ == "__main__":
                               channel_centrioles=[1, 2],
                               vicinity=50,
                               cpu=False,
-                              factor=256)
+                              )
     run(args)
