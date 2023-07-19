8415744282ed0f89e544182531329ad5941a0ca6
Leo Burgy <leo.burgy@gmail.com>
2023-07-19 14:33:27 +0200

    Update syntax

--- src/scripts/experiments/phase.py
+++ src/scripts/experiments/phase.py
@@ -18,13 +18,12 @@ np.random.seed(0)
 def main():
     data_path = Path('out/nuclei.tsv')
     if not data_path.exists():
-        ds = Dataset('/Users/buergy/Dropbox/epfl/datasets/RPE1wt_CEP152+GTU88+PCNT_1', projection_suffix='_max')
+        ds = Dataset('/Users/buergy/Dropbox/epfl/datasets/RPE1wt_CEP152+GTU88+PCNT_1')
         ds.setup()
-        ds.write_fields()
 
         data = []
         for field in ds.fields:
-            nuclei = extract_nuclei(field, channel=0, factor=256)
+            nuclei = extract_nuclei(field, channel=0)
             for n in nuclei:
                 data.append((n.intensity, n.area))
 
