c6bae6b3a631a859c34bfcb546d46026a602564f
Leo Burgy <leo.burgy@gmail.com>
2023-07-19 14:34:20 +0200

    Update tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,7 +1,6 @@
-from pathlib import Path
+from pathlib import Path, PosixPath
 
 import pytest
-
 from cenfind.core.data import Dataset, Field
 
 ROOT_DIR = Path(__file__).parent.parent
@@ -9,29 +8,36 @@ ROOT_DIR = Path(__file__).parent.parent
 
 class TestData:
     path_dataset = ROOT_DIR / "data/dataset_test"
-    field_name = "RPE1wt_CEP63+CETN2+PCNT_1_000_000"
-    dataset = Dataset(path=path_dataset, image_type=".ome.tif")
-    dataset.setup()
-    dataset.write_fields()
-    field = Field(field_name, dataset)
-    projection = field.projection
-    channel = field.channel(1)
+    dataset = Dataset(path_dataset)
+    field_name = "RPE1wt_CEP63+CETN2+PCNT_1_000_000.tif"
+    field_path = path_dataset / "projections" / field_name
+    field = Field(field_path)
+    channel = field.data[1, ...]
+    good_field_name = ROOT_DIR / "data/dataset_test/projections" / "RPE1wt_CEP63+CETN2+PCNT_1_000_001.tif"
+    bad_field_name = ROOT_DIR / "data/dataset_test/projections" / "RPE1wt_CEP63+CETN2+PCNT_1_000_001.png"
 
-    def test_write_fields(self):
-        self.dataset.write_fields()
-        assert (self.dataset.path / "fields.txt").is_file()
+    def test_bad_field_name(self):
+        with pytest.raises(ValueError):
+            Field(self.bad_field_name)
 
-    def test_field(self):
-        assert self.field.name == self.field_name
+    def test_field_data(self):
+        field = Field(self.good_field_name)
+        assert field.data.shape == (5, 2048, 2048)
 
-    def test_dataset(self):
-        assert self.dataset.path == self.path_dataset
+    def test_field_name(self):
+        assert self.field.name == self.field_name.split('.')[0]
 
     def test_projection(self):
-        assert self.projection.ndim == 3
+        assert self.field.data.ndim == 3
 
     def test_channel(self):
         assert self.channel.shape == (2048, 2048)
+
+    def test_fields(self):
+        assert self.dataset.fields == [Field(path=PosixPath(
+            '/Users/buergy/Dropbox/epfl/projects/cenfind/data/dataset_test/projections/RPE1wt_CEP63+CETN2+PCNT_1_000_000.tif')),
+            Field(path=PosixPath(
+                '/Users/buergy/Dropbox/epfl/projects/cenfind/data/dataset_test/projections/RPE1wt_CEP63+CETN2+PCNT_1_000_001.tif'))]
 
 
 class TestDataNotExisting:
