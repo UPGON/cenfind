5617071efd78b4e353593fa22d64e778c77568d0
Leo Burgy <leo.burgy@gmail.com>
2023-07-20 09:11:20 +0200

    Remove module loggers and unnecessary checking

--- src/cenfind/cli/score.py
+++ src/cenfind/cli/score.py
@@ -2,12 +2,10 @@ import argparse
 import os
 from pathlib import Path
 
-import pandas as pd
-import tifffile as tif
 from cenfind.core.data import Dataset
 from cenfind.core.detectors import extract_foci, extract_nuclei
 from cenfind.core.log import get_logger
-from cenfind.core.measure import assign, save_foci, measure_signal_foci
+from cenfind.core.measure import assign, save_foci, measure_signal_foci, save_scores
 from cenfind.core.outline import visualisation
 from tqdm import tqdm
 
@@ -47,34 +45,15 @@ def register_parser(parent_subparsers):
 
 
 def run(args):
-    if not Path(args.model).exists():
-        raise FileNotFoundError(f"{args.model} does not exist")
-
-    if args.channel_nuclei in set(args.channel_centrioles):
-        raise ValueError("Nuclei channel cannot be in channels")
-
     if args.cpu:
         os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
     dataset = Dataset(args.dataset)
-
+    dataset.setup()
     logger = get_logger(__name__, file=dataset.logs / 'cenfind.log')
 
-    channels = dataset.fields[0].data.shape[0]
-
-    if args.channel_nuclei not in range(channels):
-        logger.error("Index for nuclei (%s) out of index range" % args.channel_nuclei, exc_info=True)
-        raise ValueError
-
-    if not set(args.channel_centrioles).issubset(set(range(channels))):
-        logger.error(
-            "Channels (%s) out of channel range %s" % args.channel_centrioles,
-            set(range(channels)),
-        )
-        raise ValueError
-
-    path_visualisation_model = dataset.visualisation / args.model.name
-    path_visualisation_model.mkdir(exist_ok=True)
+    path_predictions_centrioles = dataset.predictions / "centrioles"
+    path_predictions_centrioles.mkdir(exist_ok=True)
 
     pbar = tqdm(dataset.fields)
     from cenfind.core.measure import score
@@ -91,16 +70,23 @@ def run(args):
 
         for channel in args.channel_centrioles:
             logger.info("Processing %s / %d" % (field.name, channel))
+
             foci = extract_foci(field=field, foci_model_file=args.model, channel=channel)
 
             nuclei_scored = assign(nuclei, foci, vicinity=args.vicinity)
             scored = score(field, nuclei_scored, channel)
             scores.append(scored)
 
-            predictions_path = dataset.predictions / "centrioles" / f"{field.name}_C{channel}.txt"
             measurements_path = dataset.statistics / f"{field.name}_C{channel}.txt"
-            measure_signal_foci(field, channel, foci, measurements_path)
+            measure_signal_foci(measurements_path, field, channel, foci)
+
+            predictions_path = path_predictions_centrioles / f"{field.name}_C{channel}.txt"
             save_foci(foci, predictions_path)
+
+            path_visualisation = dataset.visualisation / f"{field.name}_C{channel}_pred.png"
+            visualisation(path_visualisation, field,
+                          channel_centrioles=channel, channel_nuclei=args.channel_nuclei,
+                          nuclei=nuclei_scored)
 
             pbar.set_postfix(
                 {
@@ -110,24 +96,9 @@ def run(args):
                     "foci": len(foci),
                 }
             )
-
-            logger.info(
-                "(%s), channel %s: nuclei: %s; foci: %s"
-                % (field.name, channel, len(nuclei), len(foci))
-            )
-
-            vis = visualisation(field, channel_centrioles=channel, channel_nuclei=args.channel_nuclei,
-                                nuclei=nuclei_scored)
-            tif.imwrite(
-                path_visualisation_model / f"{field.name}_C{channel}_pred.png", vis
-            )
-
         logger.info("DONE (%s)" % field.name)
 
-    flattened = [leaf for tree in scores for leaf in tree]
-    scores_df = pd.DataFrame(flattened)
-    scores_df.to_csv(dataset.statistics / "scores_df.tsv", sep="\t", index=False)
-    logger.info("Writing raw scores to %s" % str(dataset.path / "statistics" / "scores_df.tsv"))
+    save_scores(dataset.statistics / "scores_df.tsv", scores)
 
 
 if __name__ == "__main__":
