86e625a1c9f843cc1f16c8ed9a5e5309c6d1f57a
Leo Burgy <leo.burgy@gmail.com>
2023-07-28 16:38:19 +0200

    Move field_score_frequency to analytics.py

--- src/cenfind/core/analytics.py
+++ src/cenfind/core/analytics.py
@@ -1,6 +1,7 @@
 import itertools as it
 
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 
 
@@ -41,6 +42,38 @@ def plot_layout(data, channel, ax, vmin=0, vmax=1):
     return ax
 
 
+def field_score_frequency(df, by="field"):
+    """
+    Count the absolute frequency of number of centriole per well or per field
+    :param df: Df containing the number of centriole per nuclei
+    :param by: the unit to group by, either `well` or `field`
+    :return: Df with absolut frequencies.
+    """
+    cuts = [0, 1, 2, 3, 4, 5, np.inf]
+    labels = "0 1 2 3 4 +".split(" ")
+
+    df = df.set_index(["fov", "channel"])
+    result = pd.cut(df["score"], cuts, right=False, labels=labels, include_lowest=True)
+    result = result.groupby(["fov", "channel"]).value_counts()
+    result.name = "freq_abs"
+    result = result.sort_index().reset_index()
+    result = result.rename({"score": "score_cat"}, axis=1)
+    if by == "well":
+        result[["well", "field"]] = result["fov"].str.split("_", expand=True)
+        print(result.columns)
+        result = result.groupby(["well", "channel", "score_cat"])[["freq_abs"]].sum()
+        result = result.reset_index()
+        result = result.pivot(index=["well", "channel"], columns="score_cat")
+        result.reset_index().sort_values(["channel", "well"])
+    else:
+        result = result.groupby(["fov", "channel", "score_cat"]).sum()
+        result = result.reset_index()
+        result = result.pivot(index=["fov", "channel"], columns="score_cat")
+        result.reset_index().sort_values(["channel", "fov"])
+
+    return result
+
+
 def fill_plate(data, channel, shape):
     rows, cols = shape
     for entry in data.iterrows():
@@ -53,7 +86,7 @@ def fill_plate(data, channel, shape):
 def generate_figure(data, vmin=0, vmax=1):
     shape = (8, 12)
     channels = data["channel"].unique()
-    fig, axes = plt.subplots(nrows=len(channels), ncols=1, figsize=(7*len(channels), 5))
+    fig, axes = plt.subplots(nrows=len(channels), ncols=1, figsize=(7 * len(channels), 5))
     for c, channel in enumerate(channels):
         try:
             ax = axes[c]
