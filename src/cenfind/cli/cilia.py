f44aa4de4e50dd6c8a6b0c4cde739e91dd2b3e4f
Leo Burgy <leo.burgy@gmail.com>
2023-07-28 16:38:53 +0200

    Delete superfluous check in cilia.py

--- src/cenfind/cli/cilia.py
+++ src/cenfind/cli/cilia.py
@@ -7,7 +7,6 @@ from tqdm import tqdm
 
 from cenfind.core.data import Dataset
 from cenfind.core.detectors import extract_cilia, extract_nuclei
-from cenfind.core.log import get_logger
 
 
 def register_parser(parent_subparsers):
@@ -42,39 +41,15 @@ def run(args):
         os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
     dataset = Dataset(args.dataset)
-    logger = get_logger(__name__, file=dataset.logs / 'cenfind.log')
-
-    channels = dataset.fields[0].data.shape[0]
-
-    if args.channel_nuclei not in range(channels):
-        logger.error("Index for nuclei (%s) out of index range" % args.channel_nuclei, exc_info=True)
-        raise ValueError
-
-    if args.channel_cilia not in set(range(channels)):
-        logger.error(
-            "Channels (%s) out of channel range %s" % args.channel_cilia,
-            set(range(channels)),
-        )
-        raise ValueError
-
     pbar = tqdm(dataset.fields)
 
     proportions = []
     for field in pbar:
         pbar.set_description(f"{field.name}")
-        logger.info("Processing %s" % field.name)
-
         nuclei = extract_nuclei(field, args.channel_nuclei)
-        if len(nuclei) == 0:
-            logger.warning("No nucleus has been detected in %s" % field.name)
-            continue
-        logger.info("Processing %s / %d" % (field.name, args.channel_cilia))
         path_cilia = (dataset.visualisation / "cilia")
         path_cilia.mkdir(exist_ok=True)
         cilia = extract_cilia(field, channel=2, dst=path_cilia)
-        if len(cilia) == 0:
-            logger.warning("No centrioles (channel: %s) has been detected in %s" % (args.channel_cilia, field.name))
-
         ciliated = len(cilia) / len(nuclei)
         proportions.append({'field': field.name,
                             "channel_cilia": args.channel_cilia,
@@ -90,20 +65,8 @@ def run(args):
             }
         )
 
-        logger.info(
-            "(%s), channel %s: nuclei: %s; foci: %s"
-            % (field.name, args.channel_cilia, len(nuclei), len(cilia))
-        )
-
-        logger.info(
-            "Writing visualisations for (%s), channel %s" % (field.name, args.channel_cilia)
-        )
-
-        logger.info("DONE (%s)" % field.name)
-
     proportions_df = pd.DataFrame(proportions)
     proportions_df.to_csv(dataset.statistics / "proportions_df.tsv", sep="\t", index=False)
-    logger.info("Writing raw proportions to %s" % str(dataset.statistics / "proportions_df.tsv"))
 
 
 if __name__ == "__main__":
