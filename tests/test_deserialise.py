94ee0b9b6fbfe873c6df5b51815362bc27669fd0
Leo Burgy <leo.burgy@gmail.com>
2023-09-06 17:08:38 +0200

    Fix test

--- tests/test_deserialise.py
+++ tests/test_deserialise.py
@@ -7,7 +7,7 @@ def test_load_contours():
     with open('/Users/buergy/Dropbox/epfl/projects/cenfind/data/dataset_test/predictions/nuclei/RPE1wt_CEP63+CETN2+PCNT_1_000_000_C0.json', 'r') as f:
         contours = json.load(f)
     assert type(contours) == dict
-    cnts = [np.array(cnt) for cnt in contours.values()]
+    cnts = [np.array(cnt['contour']) for cnt in contours.values()]
     bg = np.zeros((2048, 2048), dtype='uint8')
     cv2.drawContours(bg, cnts, -1, 255, -1)
     cv2.imwrite('../out/test.png', bg)
