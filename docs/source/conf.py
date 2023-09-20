73dfd7a41e3b4eb7a5f00f68519e18bbdde04e32
Leo Burgy <leo.burgy@gmail.com>
2023-09-20 15:43:19 +0200

    Add version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,8 @@
 project = 'CenFind'
 copyright = '2023, Leo Burgy'
 author = 'Leo Burgy'
-release = '0.1.0'
+release = '0.13.1'
+version = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
