1f2976866b450fd671fc9e7b455fd7a749bef50e
Leo Burgy <leo.burgy@gmail.com>
2023-10-11 15:09:23 +0200

    Draft analyse program for statistics plots

---
+++ src/cenfind/cli/analyse.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from pathlib import Path
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+
+def main():
+    sns.set_theme()
+    working_dir = Path('/Users/buergy/Downloads/RPE1wt_CEP63+CETN2+PCNT_1')
+    # Concatenate the predictions and keep identity of field of view
+    files = Path(working_dir / 'predictions/centrioles').glob('*.tsv')
+    dfs = list()
+    for f in files:
+        data = pd.read_csv(f, sep='\t')
+        data['file'] = f.stem
+        dfs.append(data)
+
+    df = pd.concat(dfs, ignore_index=True)
+
+    # Plot the intensities
+    g = sns.displot(df, x='intensity', hue='channel')
+    g.savefig(working_dir / 'visualisation/test.png')
+
+    # Plot the scores
+    data = pd.read_csv(working_dir / 'statistics/statistics.tsv', sep='\t')
+    summary = data.groupby('channel').sum(numeric_only=True)
+    summary = pd.wide_to_long(summary.reset_index(), stubnames='', i='channel', j='number', suffix='(\d)|\+')
+    summary.columns = ['frequency']
+    summary = summary.reset_index()
+    g = sns.catplot(summary, x='number', y='frequency', hue='channel', kind='bar')
+    g.savefig(working_dir / 'visualisation/test2.png')
+
+    # Plot SA/I of nuclei
+    files = Path(working_dir / 'predictions/nuclei').glob('*.json')
+    dfs = list()
+    for f in files:
+        data = pd.read_json(f, orient='index')
+        data = data.drop('contour', axis=1)
+        data['file'] = f.stem
+        dfs.append(data)
+    df = pd.concat(dfs, ignore_index=True)
+    df = df.loc[df['is_nucleus_full']]
+
+    f, ax = plt.subplots(figsize=(6.5, 6.5))
+    sns.scatterplot(data=df, x='surface_area', y='intensity', ax=ax)
+    sns.rugplot(data=df, x='surface_area', y='intensity', ax=ax)
+    f.savefig(working_dir / 'visualisation/test3.png')
+
+    print(df)
+
+
+if __name__ == '__main__':
+    main()
