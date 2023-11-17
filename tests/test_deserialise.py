03afb9c67d13117c15a9553efda8c9a31e538270
Leo Burgy <leo.burgy@gmail.com>
2023-11-17 10:19:18 +0100

    Fix error in test

--- tests/test_deserialise.py
+++ tests/test_deserialise.py
@@ -7,7 +7,7 @@ def test_load_contours():
     with open('data/cilia/predictions/nuclei/RPE1-GFPCep135_NC_48h_100k_mCETN2-AF488_rArl13b-AF568_gCEP164-AF647_1_MMStack_10-Pos_000_001_max_C0.json', 'r') as f:
         contours = json.load(f)
     assert type(contours) == dict
-    cnts = [np.array(cnt['contour']) for cnt in contours.values()]
+    cnts = [np.array(cnt['contour']) for cnt in contours['nuclei'].values()]
     bg = np.zeros((2048, 2048), dtype='uint8')
     cv2.drawContours(bg, cnts, -1, 255, -1)
     cv2.imwrite('../out/test.png', bg)
