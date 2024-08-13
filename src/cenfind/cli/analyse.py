5f6c238301eabb9ddde4f43d79a51100f041a3be
Leo Burgy <leo.burgy@epfl.ch>
2024-08-13 13:55:13 +0200

    Fix analyse.py

--- src/cenfind/cli/analyse.py
+++ src/cenfind/cli/analyse.py
@@ -1,14 +1,20 @@
+import argparse
+import logging
+
 import pandas as pd
 from pathlib import Path
 import seaborn as sns
 from matplotlib import pyplot as plt
 
+from cenfind.core.data import Dataset
+logging.basicConfig(level=logging.ERROR)
 
-def main():
+def main(args=None):
+
     sns.set_theme()
-    working_dir = Path('/Users/buergy/Downloads/IF_2022_09_07_U2OS_WTasync')
+    dataset = Dataset(args.dataset)
     # Concatenate the predictions and keep identity of field of view
-    files = Path(working_dir / 'predictions/centrioles').glob('*.tsv')
+    files = Path(dataset.predictions / "centrioles").glob('*.tsv')
     dfs = list()
     for f in files:
         data = pd.read_csv(f, sep='\t')
@@ -19,23 +25,23 @@ def main():
 
     # Plot the intensities
     g = sns.displot(df, x='intensity', hue='channel')
-    g.savefig(working_dir / 'visualisation/test.png')
+    g.savefig(dataset.statistics / "vis_intensities.png")
 
     # Plot the scores
-    data = pd.read_csv(working_dir / 'statistics/statistics.tsv', sep='\t')
+    data = pd.read_csv(dataset.statistics / "statistics.tsv", sep='\t')
     summary = data.groupby('channel').sum(numeric_only=True)
     summary = pd.wide_to_long(summary.reset_index(), stubnames='', i='channel', j='number', suffix='(\d)|\+')
     summary.columns = ['frequency']
     summary = summary.reset_index()
     g = sns.catplot(summary, x='number', y='frequency', hue='channel', kind='bar')
-    g.savefig(working_dir / 'visualisation/test2.png')
+    g.savefig(dataset.visualisation / 'vis_scores.png')
 
     # Plot SA/I of nuclei
-    files = Path(working_dir / 'predictions/nuclei').glob('*.json')
+    files = Path(dataset.predictions / 'nuclei').glob('*.json')
     dfs = list()
     for f in files:
-        data = pd.read_json(f, orient='index')
-        data = data.drop('contour', axis=1)
+        data = pd.read_json(f)
+        data = pd.DataFrame(data["nuclei"].values.tolist()).drop("contour", axis=1)
         data['file'] = f.stem
         dfs.append(data)
     df = pd.concat(dfs, ignore_index=True)
@@ -44,14 +50,13 @@ def main():
     f, ax = plt.subplots(figsize=(6.5, 6.5))
     sns.scatterplot(data=df, x='surface_area', y='intensity', ax=ax)
     sns.rugplot(data=df, x='surface_area', y='intensity', ax=ax)
-    f.savefig(working_dir / 'visualisation/test3.png')
+    f.savefig(dataset.visualisation / 'vis_surface_area_intensity.png')
 
     f, ax = plt.subplots(figsize=(6.5, 6.5))
     sns.histplot(data=df, x='intensity', ax=ax, kde=True)
-    f.savefig(working_dir / 'visualisation/test4.png')
-
-    print(df)
+    f.savefig(dataset.visualisation / 'vis_intensity.png')
 
 
 if __name__ == '__main__':
-    main()
+    args = argparse.Namespace(dataset=Path("../../../data/dataset_test"))
+    main(args)
